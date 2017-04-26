55252c31660dfcf4874a5f4d508bfd0f697e6345
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2017-04-26 15:51:39 +0300

    Added different types feedbacks parsing.

--- delivery/parser.py
+++ delivery/parser.py
@@ -1,42 +1,48 @@
 import lxml.html as html
 import feedback
 import math
+import re
 
 from curl.curl import Curl
+
+class FeedbacksType:
+    NEGATIVE = 0
+    POSITIVE = 1
 
 class DeliveryFeedbackParser:
     def __init__(self):
         pass
 
-    def receiveFeedbacks(self, restaurantId):
-        pagesAmount = self.countFeedbacksPages(restaurantId)
-        return self.parseFeedbacksPages(restaurantId, pagesAmount)
+    def receiveFeedbacks(self, restaurantId, feedbacksType = FeedbacksType.POSITIVE):
+        pagesAmount = self.countFeedbacksPages(restaurantId, feedbacksType)
+        return self.parseFeedbacksPages(restaurantId, feedbacksType, pagesAmount)
 
-    def parseFeedbacksPages(self, restaurantId, pagesAmount):
-        feedbacks = []
-        for pageId in range(1, pagesAmount + 1):
-            pageTree = self.receivePageTree(restaurantId, pageId)
-            if pageTree is not None: feedbacks.extend(self.deriveFeedbacks(pageTree))
-        return feedbacks
-
-    def countFeedbacksPages(self, restaurantId):
-        mainPageTree = self.receivePageTree(restaurantId)
+    def countFeedbacksPages(self, restaurantId, feedbacksType):
+        mainPageTree = self.receivePageTree(restaurantId, feedbacksType)
         if mainPageTree is not None:
-            pageTabs = mainPageTree.find_class("tabs").pop()
-            feedbacksPages = pageTabs.find_class("active").pop()
-            feedbacksPages = int(feedbacksPages.xpath("a/span/text()").pop())
+            contentSection = mainPageTree.get_element_by_id("content")
+            feedbacksPages = contentSection.xpath("div[last()]/div/h1/span/text()").pop()
+            feedbacksPages = int(re.findall(r"(\d+)", feedbacksPages).pop())
             return math.ceil(feedbacksPages / 20)
         else:
             return 0
 
-    def receivePageTree(self, restaurantId, pageId = 1):
-        restaurantUrl = self.prepareRestaurantUrl(restaurantId, pageId)
+    def parseFeedbacksPages(self, restaurantId, feedbacksType, pagesAmount):
+        feedbacks = []
+        for pageId in range(1, pagesAmount + 1):
+            pageTree = self.receivePageTree(restaurantId, feedbacksType, pageId)
+            if pageTree is not None: feedbacks.extend(self.deriveFeedbacks(pageTree))
+        return feedbacks
+
+    def receivePageTree(self, restaurantId, feedbacksType, pageId = 1):
+        restaurantUrl = self.prepareRestaurantUrl(restaurantId, feedbacksType, pageId)
         pageContent = Curl().sendGetRequest(restaurantUrl)
         return html.fromstring(pageContent) if pageContent else None
 
-    def prepareRestaurantUrl(self, restaurantId, pageId):
-        restaurantUrl = "http://www.delivery-club.ru/srv/{}/feedbacks/page/{}"
-        return restaurantUrl.format(restaurantId, pageId)
+    def prepareRestaurantUrl(self, restaurantId, feedbacksType, pageId):
+        restaurantUrl = "http://www.delivery-club.ru/srv/{}/feedbacks/{}/page/{}"
+        feedbacksType = "positive" if feedbacksType == FeedbacksType.POSITIVE else "negative"
+        return restaurantUrl.format(restaurantId, feedbacksType, pageId)
 
     def deriveFeedbacks(self, pageTree):
         feedbacksList = pageTree.find_class("feedbacks_list").pop()
